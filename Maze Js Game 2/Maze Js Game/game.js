8702ab8923b0e9f4bbfce635d2c3c0636e28d38d
larissa-abboud <lariabboud@gmail.com>
2022-09-17 20:20:35 +0300

    js code

--- Maze Js Game 2/Maze Js Game/game.js
+++ Maze Js Game 2/Maze Js Game/game.js
@@ -1,22 +1,124 @@
 window.onload=function(){
-var m = document.getElementById("start");
-let flag = true;
-m.addEventListener("mouseover", M_over);
+let m = document.getElementById("start");
+let m1 = document.getElementsByClassName("boundary");
+let m2 = document.getElementById("end");
+let m3 = document.getElementById("game");
+let flag_begin = false;
+let flag_end = false;
+let counter_start = 0;
 
-var m1 = document.getElementsByClassName("boundary");
+
+// start game
+/**
+ * mouse hover over start, 
+ * if mouse out then mouse over end 
+ * if mouse out th
+ */
 
 
-for (var i = 0 ; i < m1.length; i++) {
-   m1[0].addEventListener("mouseover", M_over_border) ;
+
+ m.addEventListener("mouseover", M_over)
+
+
+m.addEventListener("mouseleave",M_leave_start);
+if(counter_start == 0){
+
  
-}
-
-
-}
-function M_over() {
-  console.log("Moused over!") ;// use counter and use if mouse out condion
+  if(m3.addEventListener("mouseleave",M_out)){
+    alert("Game over")}
+    else{
+      for (var i = 0 ; i < m1.length; i++) {
+      m1[i].addEventListener("mouseover", M_over_border) ;
+    
+    }
+    m2.addEventListener("mouseover",M_over_end);
+    if(flag_end){
+      for (var i = 0 ; i < m1.length; i++) {
+        m1[i].removeEventListener("mouseover", M_end_event) ;
+      
+    }
   }
+  } }
+  counter_start++;
+  flag(counter_start);
+  //console.log(counter_start)
+
+
+/*if ( !isEventListenerActive){
+  console.log(flag_begin)
+  if(m3.addEventListener("mouseleave",M_out)){
+  alert("Game over")}
+  else{
+    for (var i = 0 ; i < m1.length; i++) {
+    m1[i].addEventListener("mouseover", M_over_border) ;
+  
+  }
+  m2.addEventListener("mouseover",M_over_end);
+  if(flag_end){
+    for (var i = 0 ; i < m1.length; i++) {
+      m1[i].removeEventListener("mouseover", M_end_event) ;
+    
+  }
+}
+}
+}*/
+
+
+
+}
+function M_out(){
+  console.log("you lost");
+}
+function M_over(flag_begin) {
+  flag_begin = true;
+  
+  console.log("start") ;// use counter and use if mouse out condion
+  
+ 
+  }
+function M_leave_start(){
+  flag_begin = true;
+
+}
 function M_over_border() {
-  console.log("game over ") ;
+  console.log("you lose ") ;
 
   }
+function M_over_end(){
+  flag_end = true;
+  console.log("you win");console.log(flag_end);
+}
+function M_end_event(){
+
+  console.log("removed");
+  
+}
+function flag(counter){
+  return counter;
+}
+function begin(  m1 , m2, m3 ,flag_end,counter_start){
+  console.log(counter_start)
+  flag(counter_start);
+  if(counter_start == 0){
+
+ 
+  if(m3.addEventListener("mouseleave",M_out)){
+    alert("Game over")}
+    else{
+      for (var i = 0 ; i < m1.length; i++) {
+      m1[i].addEventListener("mouseover", M_over_border) ;
+    
+    }
+    m2.addEventListener("mouseover",M_over_end);
+    if(flag_end){
+      for (var i = 0 ; i < m1.length; i++) {
+        m1[i].removeEventListener("mouseover", M_end_event) ;
+      
+    }
+  }
+  } }
+  counter_start++;
+  flag(counter_start);
+  console.log(counter_start)
+}
+
